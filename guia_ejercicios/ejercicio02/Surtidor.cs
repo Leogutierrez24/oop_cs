8c50c07cdaa5f6af4cd02ee85bf7201282384a05
Leogutierrez24 <leoalegutierrez97@gmail.com>
2023-04-27 12:03:57 -0300

    inicializando el ejercicio 2. Nueva clase Surtidor y Venta

---
+++ guia_ejercicios/ejercicio02/Surtidor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio02
+{
+    public class Surtidor
+    {
+        public const decimal LIMITE = 100;
+
+        private string _nafta;
+        private decimal _precio;
+        private decimal _cantidad;
+
+        public string Nafta
+        {
+            get { return _nafta; } 
+            set {  _nafta = value; }
+        }
+
+        public decimal Precio
+        {
+            get { return _precio; }
+            set 
+            {
+                if (value > 0)
+                {
+                    _precio = value;
+                }
+            }
+        }
+
+        public decimal Cantidad
+        {
+            get { return _cantidad; }
+            set
+            {
+                if (value <= LIMITE && value >= 0)
+                {
+                    _cantidad = value;
+                }
+            }
+        }
+
+        public void Descargar(decimal cantDescarga)
+        {
+            if (this._cantidad <= 0 || cantDescarga > this._cantidad)
+            {
+                MessageBox.Show("El surtidor esta vacío o no dispone esa cantidad!");
+            } else
+            {
+                this._cantidad -= cantDescarga;
+                MessageBox.Show($"Se descargaron {cantDescarga} Litros!");
+            }
+        }
+
+        public void Cargar()
+        {
+            this._cantidad = 100;
+            MessageBox.Show("Surtidos cargado!");
+        }
+    }
+}
---
+++ guia_ejercicios/ejercicio02/Venta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio02
+{
+    public class Venta
+    {
+        public Venta(string tipo, decimal cantidad, decimal total)
+        {
+            this.Fecha = DateTime.Now;
+            this.Tipo = tipo;
+            this.Cantidad = cantidad;
+            this.Total = total;
+        }
+
+        public DateTime Fecha { get; private set; }
+        public string Tipo { get; set; }
+        public decimal Cantidad { get; set; }
+        public decimal Total { get; set; }
+
+    }
+}
