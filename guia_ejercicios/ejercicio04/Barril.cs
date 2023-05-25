8efb1fa7d38b18af6606e73a90206ee81bc5727b
Leogutierrez24 <leoalegutierrez97@gmail.com>
2023-05-24 22:42:59 -0300

    agregado el ejericio 4. Clases creadas, no todas estan desarrollas todavia

---
+++ guia_ejercicios/ejercicio04/Barril.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio04
+{
+    public class Barril
+    {
+        private Cerveza _cerveza;
+        private float _capacidad;
+        private float _recaudacion;
+        private int _ventas;
+
+        public Cerveza Cerveza
+        {
+            get { return _cerveza; }            
+        }
+
+        public float Capacidad
+        {
+            get { return _capacidad; }
+        }
+
+        public float Recaudacion
+        {
+            get { return _recaudacion; }
+        }
+
+        public int Ventas
+        {
+            get { return _ventas; }
+        }
+
+        public Barril(Cerveza cerveza)
+        {
+            this._cerveza = cerveza;
+            this._capacidad = 0;
+            this._recaudacion = 0;
+            this._ventas = 0;
+        }
+
+        public void AgregarVenta(float total)
+        {
+            this._recaudacion += total;
+            this._ventas++;
+        }
+    }
+}
